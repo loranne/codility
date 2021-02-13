6296c02d3bd35f7573cf194ab25956160f95f847
Loranne Nasir <loranne.n@gmail.com>
2021-02-12 16:45:03 -0800

    binary gap progress

--- binary_gap.py
+++ binary_gap.py
@@ -5,8 +5,14 @@ def solution(N):
 
     gaps = binary_n.split("1")
 
+    # if binary ends with a zero, like bin(32), should return zero
+    # but only need to discount the one before it!
     if gaps[-1] != "":
         return 0
+
+    if gaps[-1] != "":
+        gaps[len(gaps) - 2] = 0
+        print(gaps[len(gaps) - 2])
 
     count = 0
 
@@ -15,3 +21,6 @@ def solution(N):
             count = len(x)
     
     return count
+
+print(solution(32))
+print(solution())
